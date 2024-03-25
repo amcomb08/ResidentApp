013faf5db5867e5514c2a6321270d837050e2e2f
amcomb08 <amcomb08@louisville.edu>
2024-03-25 14:25:54 -0500

    Resident Header
    Added generation of resident header

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,3 @@
-// Burger menus
-document.addEventListener('DOMContentLoaded', function() {
-    // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
-
-    if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-
-    // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-    if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-
-    if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-});
-
 // Define the logout function
 function logoutUser() {
     fetch('http://localhost:5000/logout', { credentials: 'include', method: 'POST' })
@@ -214,6 +173,123 @@ function createAdminHeader() {
 
     // Return the header container
     return headerContainer;
+}
+
+
+function createHeader() {
+    const headerContainer = document.createElement('div');
+    headerContainer.className = 'hidden xl:block w-full md:w-auto px-2 mr-auto';
+
+    const ul = document.createElement('ul');
+    ul.className = 'flex items-center';
+
+    // Define the menu items
+    const menuItems = [
+        { text: 'Home', href: 'index.html' },
+        { text: 'Messages', href: 'messages.html'},
+        { text: 'Payment History', href: 'ledger.html' },
+        { text: 'Community', href: 'community.html' },
+        {
+            text: 'All Options',
+            dropdown: [
+                { text: 'Change Password', href: 'changepasswordloggedin.html' },
+            ]
+        }
+    ];
+
+    menuItems.forEach((item) => {
+        const li = document.createElement('li');
+        li.className = 'mr-10';
+
+        if (item.dropdown) {
+            const a = document.createElement('a');
+            a.className = 'inline-block text-sm font-semibold text-gray-300 hover:text-gray-200 cursor-pointer';
+            a.id = item.text.toLowerCase().replace(/ /g, '-') + '-link'; // Generate an id from text
+            a.textContent = item.text;
+            li.appendChild(a);
+
+            const dropdownMenu = document.createElement('div');
+            dropdownMenu.className = 'dropdown-menu absolute hidden bg-gray-700 text-white';
+
+            item.dropdown.forEach((dropdownItem) => {
+                const dropdownLink = document.createElement('a');
+                dropdownLink.className = 'block px-4 py-2 text-sm text-gray-200 hover:bg-gray-600';
+                dropdownLink.href = dropdownItem.href;
+                dropdownLink.textContent = dropdownItem.text;
+                dropdownMenu.appendChild(dropdownLink);
+            });
+
+            // Append the dropdown menu to the list item
+            li.appendChild(dropdownMenu);
+        } else {
+            const a = document.createElement('a');
+            a.className = 'inline-block text-sm font-semibold text-gray-300 hover:text-gray-200';
+            a.href = item.href;
+            a.textContent = item.text;
+
+            // Add badge if needed
+            if (item.badge) {
+                const badgeDiv = document.createElement('div');
+                badgeDiv.className = 'flex w-5 h-5 ml-2 items-center justify-center text-xs text-white bg-blue-500 rounded-full';
+                badgeDiv.textContent = item.badge;
+                const span = document.createElement('span');
+                span.appendChild(badgeDiv);
+                a.appendChild(span);
+            }
+
+            li.appendChild(a);
+        }
+
+        // Append the list item to the unordered list
+        ul.appendChild(li);
+    });
+
+    // Create a container for the logout button
+    const logoutButtonContainer = document.createElement('div');
+    logoutButtonContainer.className = 'w-full sm:w-auto px-3 mb-4';
+
+    // Create the anchor tag for the logout button
+    const logoutButtonAnchor = document.createElement('a');
+    // Set the href if you're using a link to log out
+    logoutButtonAnchor.href = '#'; // Set this to your logout script or leave it to be handled by JS
+
+    // Create the logout button
+    const logoutButton = document.createElement('button');
+    logoutButton.id = 'logoutButton';
+    logoutButton.className = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
+    logoutButton.textContent = 'Sign Out';
+
+    // Append the button to the anchor tag, and then the anchor tag to the button container
+    logoutButtonAnchor.appendChild(logoutButton);
+    logoutButtonContainer.appendChild(logoutButtonAnchor);
+
+    // Append the logout button container to the header
+    ul.appendChild(logoutButtonContainer);
+
+    // Append the unordered list to the header container
+    headerContainer.appendChild(ul);
+
+    // Return the header container
+    return headerContainer;
+}
+
+function initalizeNavBar(){
+    // Create the header and append it to the body
+    document.getElementById('main-header').appendChild(createHeader());
+    // Use the logoutUser function from navbar.js when the logout button is clicked
+    document.getElementById('logoutButton').addEventListener('click', logoutUser);
+
+    document.getElementById('all-options-link').addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevent click from immediately propagating to document
+        showDropdown('all-options-link');
+    });
+
+    document.addEventListener('click', function(event) {
+        var dropdownMenu = document.querySelector('#all-options-link + .dropdown-menu');
+        if (dropdownMenu.style.display === 'block' && !dropdownMenu.contains(event.target) && event.target.id !== 'all-options-link') {
+            dropdownMenu.style.display = 'none'; // Hide the dropdown
+        }
+    });
 }
 
 
